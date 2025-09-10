cafdf8d61dddb52cdedc7c142dab23d5e3a848d5
Ronald-Zeballos <a2023115795@estudiantes.upsa.edu.bo>
2025-09-10 18:00:08 -0400

    hola1

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { WhatsappController } from './whatsapp.controller';
 import { WhatsappService } from './whatsapp.service';
-import { AssemblyaiModule } from '../assemblyai/assemblyai.module';
 
 @Module({
-  imports: [AssemblyaiModule],
   controllers: [WhatsappController],
   providers: [WhatsappService],
 })
